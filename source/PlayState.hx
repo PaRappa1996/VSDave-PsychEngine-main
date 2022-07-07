a6bed3615db21d9a91832925e11e55516d798e7c
PaRappa1996 <92826442+PaRappa1996@users.noreply.github.com>
2022-07-07 17:36:12 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -63,16 +63,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['IM GONNA BREAK MAH PHONE', 0.2], //From 0% to 19%
-		['Bamballs says you suck bitch', 0.4], //From 20% to 39%
-		['Bambi is not happy with this', 0.5], //From 40% to 49%
+		['You Suck!', 0.2], //From 0% to 19%
+		['Shit', 0.4], //From 20% to 39%
+		['Bad', 0.5], //From 40% to 49%
 		['Bruh', 0.6], //From 50% to 59%
-		['Eeeeh, ok!', 0.69], //From 60% to 68%
-		['Nice, 69 lol', 0.7], //69%
-		['Amazing!', 0.8], //From 70% to 79%
-		['Cool', 0.9], //From 80% to 89%
-		['dave moment', 1], //From 90% to 99%
-		['Badass', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Meh', 0.69], //From 60% to 68%
+		['Nice', 0.7], //69%
+		['Good!', 0.8], //From 70% to 79%
+		['Great!', 0.9], //From 80% to 89%
+		['Sick!!', 1], //From 90% to 99%
+		['Perfect!!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	
@@ -681,7 +681,7 @@ switch (curStage)
 				testshader.waveSpeed = 2;
 				bg.shader = testshader.shader;
 				curbg = bg;
-			}
+			
 
 				case 'bambisFarm': //Week Bambi
 				var bg:BGSprite = new BGSprite('sky', -600, -200, 0.9, 0.9);
@@ -859,7 +859,7 @@ switch (curStage)
 				testshader.waveSpeed = 2;
 				bg.shader = testshader.shader;
 				curbg = bg;
-			}
+			
 
 				case 'glitchy-void': //Week UNFAIR
 				var bg:BGSprite = new BGSprite('scarybg', -600, -200, 0.9, 0.9);
@@ -873,7 +873,7 @@ switch (curStage)
 				testshader.waveSpeed = 2;
 				bg.shader = testshader.shader;
 				curbg = bg;
-			}
+	
 
 		}
 
